0b54383dca18037838bec887e6c64e107d5013ef
hoanghai86 <hainguyenlehoang86.gmail.com>
2022-11-14 09:15:57 +0700

    ''

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./App.css";
+// import ProductList from "./Ex3/ProductList";
+// import ProductList from "./Ex3Ver1/ProductList";
+
+// import ProductList from "./components/ProductList";
+// import StudentList from "./components/StudentList";
+// import MoviesList from "./components/MoviesList";
+// import DemoDataBinding from "./components/DemoDataBinding";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Home from "./Ex1/Home";
+// import BaiTapLayOut from "./BaiTapThucHanhLayout/Home";
+// import HomeEx2 from "./Ex2/Home";
+// import ShopGlass from "./Ex2/ShopGlass"
+// import Home from "./Ex4/Home"
+import MovieBooking from "./Ex5/MovieBooking";
+class App extends React.Component {
+  render() {
+    return (
+      //các thẻ này gọi là react element, các thẻ này sẽ build thành từng obj, tất cả object thì gọi chung là virtual DOM (DOM ảo), sau khi chuyển thành DOM ảo thì sẽ build ra cây DOM thật (real DOM). Hồi đó giờ chúng ta dùng document.getelementById nghĩa là chúng ta đang tương tác với cây DOM thật, ví dụ: khi sửa 1 thuộc tính thẻ a, chúng ta .style = "color: red", mặc dù chỉ sửa 1 thẻ a nhưng sẽ render lại toàn bộ giao diện WEB, còn bên React lúc này sẽ sửa mỗi cái đối tượng thẻ a bên virtual DOM, sau đó nó so sánh với Real DOM và render lại đúng thẻ a
+
+      <div>
+        {/* <button className="btn">test style module</button> */}
+        {/* <DemoDataBinding /> */} {/* mấy cái ở đây là các component */}
+        {/* <Home /> */}
+        {/* <BaiTapLayOut /> */}
+        {/* <HomeEx2 /> */}
+        {/* <ShopGlass /> */}
+        {/* <ProductList /> */}
+        {/* <MoviesList /> */}
+        {/* <StudentList /> */}
+        {/* <Home /> */}
+        <MovieBooking />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore, combineReducers } from "redux"; //cài redux ngoài index.js, cung cấp những cái lệnh cần thiết để tạo cái store
+import { Provider } from "react-redux"; //là component của react-redux, làm trung gian để kết nối store với các component
+import categoryReducer from "./Ex4/redux/categoryReducer";
+import productReducer from "./Ex4/redux/productReducer";
+import modelReducer from "./Ex4/redux/modelReducer";
+import chairReducer from "./Ex5/redux/chairReducer"
+
+//reducer tổng có tác dụng quản lý các reducer con, khai báo tên các reducer con cho thằng root
+const rootReducer = combineReducers({
+  //Ex4
+  category: categoryReducer,
+  product: productReducer,
+  model: modelReducer,
+
+  //Ex5
+  chair: chairReducer,
+});
+
+//để thấy được store, ta cài extension Redux, sau đó vào link: https://github.com/zalmoxisus/redux-devtools-extension, và copy đoạn code: window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() bỏ vào createStore để show cái store, mục đích để kiểm tra store có hoạt động hay chưa
+//F12 bên giao diện, tab Redux, tab State để kiểm tra có category chưa
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
